a059e83cef8275888efb97a17c473a16fab48334
VincentPFG <vincentgiraud@live.fr>
2025-05-06 10:35:15 +0200

    component + small caps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
-
 import netlify from "@astrojs/netlify";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [htmx()],
+  integrations: [htmx(), alpinejs()],
   output: "server",
   adapter: netlify()
 });
